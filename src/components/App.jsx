133623c3ca0ce49e990aed017f3ebfdc84f5a889
Alex Wiegand <72582154+Wiegand92@users.noreply.github.com>
2021-04-14 22:32:36 -0400

    make more obvious change for test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <ChakraProvider>
       <div className="app">
-        <h1>Hello Gang!</h1>
+        <h1>Hello World!</h1>
         <p>Check out the README <a href='https://github.com/Wiegand92/RegexSite'>here</a></p>
       </div>
     </ChakraProvider>
